f7fc7aee042db69f3d4eecf94b22c18b38747471
Anton Shvetz <tz@coneforest.msk.ru>
2014-06-16 02:58:20 +0400

    Minor fixes

--- sources/java/coneforest/psi/OperandStack.java
+++ sources/java/coneforest/psi/OperandStack.java
@@ -2,15 +2,4 @@ package coneforest.psi;
 
 public class OperandStack extends java.util.Stack<PSIObject>
 {
-	/*
-	public PSIObject pop()
-	{
-		return super.pop().cast();
-	}
-
-	public PSIObject peek()
-	{
-		return super.peek().cast();
-	}
-	*/
 }
