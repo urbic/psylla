af328b3edd2ce8cea898f9f0ad42463faebc127e
Anton Shvetz <tz@coneforest.msk.ru>
2014-06-24 03:43:06 +0400

    "repeat" operator implemented

---
+++ sources/java/coneforest/psi/systemdict/_repeat.java
@@ -0,0 +1,40 @@
+package coneforest.psi.systemdict;
+import coneforest.psi.*;
+
+public class _repeat extends PSIOperator
+{
+	public String getName()	{ return "repeat"; }
+
+	public void execute(PSIInterpreter interpreter)
+	{
+		OperandStack opstack=interpreter.getOperandStack();
+		if(opstack.size()<2)
+		{
+			interpreter.error("stackunderflow");
+			return;
+		}
+		PSIObject obj=opstack.pop();
+		PSIObject count=opstack.pop();
+
+		if(count instanceof PSIInteger)
+		{
+			interpreter.pushLoopLevel();
+			// TODO: reverse
+			for(int i=0;
+					i<((PSIInteger)count).getValue() && !interpreter.getExitFlag();
+					i++)
+			{
+					//int level=interpreter.getExecutionStack().size();
+				//int currentLoopLevel=interpreter.getLoopLevel();
+				obj.invoke(interpreter);
+				//System.out.println("UUU "+currentLoopLevel);
+				interpreter.handleExecutionStack(interpreter.currentLoopLevel());
+				//interpreter.setLoopLevel(currentLoopLevel);
+			}
+			interpreter.setExitFlag(false);
+
+		}
+		else
+			interpreter.error("typecheck");
+	}
+}
