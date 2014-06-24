93d615b53b77c76ba70c6e3351210ece763eb8b4
Anton Shvetz <tz@sectorb.msk.ru>
2014-06-24 04:47:25 +0400

    "lt", "ge", "le", "gt" operators implemented

---
+++ sources/java/coneforest/psi/systemdict/_lt.java
@@ -0,0 +1,26 @@
+package coneforest.psi.systemdict;
+import coneforest.psi.*;
+
+public class _lt extends PSIOperator
+{
+	public String getName()	{ return "lt"; }
+
+	public void execute(PSIInterpreter interpreter)
+	{
+		OperandStack opstack=interpreter.getOperandStack();
+		if(opstack.size()<2)
+		{
+			interpreter.error("stackunderflow");
+			return;
+		}
+		PSIObject obj2=opstack.pop();
+		PSIObject obj1=opstack.pop();
+
+		if(obj1 instanceof PSINumeric && obj2 instanceof PSINumeric)
+			opstack.push(PSINumeric.lt((PSINumeric)obj1, (PSINumeric)obj2));
+		else if(obj1 instanceof PSIStringlike && obj2 instanceof PSIStringlike)
+			opstack.push(PSIStringlike.lt((PSIStringlike)obj1, (PSIStringlike)obj2));
+		else
+			interpreter.error("typecheck");
+	}
+}
