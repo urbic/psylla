1fe62118bc4b7bcac0c5bf45d89f9c77597732f8
Anton Shvetz <tz@sectorb.msk.ru>
2014-07-06 02:32:51 +0400

    "cvs" operator implemented

---
+++ src/java/coneforest/psi/systemdict/_cvs.java
@@ -0,0 +1,27 @@
+package coneforest.psi.systemdict;
+import coneforest.psi.*;
+
+public class _cvs extends PsiOperator
+{
+	public void execute(Interpreter interpreter)
+	{
+		OperandStack opstack=interpreter.getOperandStack();
+		if(opstack.size()<1)
+		{
+			interpreter.error("stackunderflow");
+			return;
+		}
+		PsiObject obj=opstack.pop();
+		opstack.push(new PsiString(obj.toString()));
+		/*
+		if(obj instanceof PsiBoolean)
+			opstack.push(new PsiString(((PsiBoolean)obj).toString()));
+		else if(obj instanceof PsiNumeric)
+			opstack.push(new PsiString(((PsiNumeric)obj).toString()));
+		else if(obj instanceof PsiStringlike)
+			opstack.push(new PsiString(((PsiStringlike)obj).toString()));
+		else
+			interpreter.error("typecheck");
+		*/
+	}
+}
