8e7205f87b59b518e3b1b8d7d5ec21e6b411141e
Anton Shvetz <tz@sectorb.msk.ru>
2014-07-06 02:41:22 +0400

    PsiInput, PsiOutput classes implemented

---
+++ src/java/coneforest/psi/PsiOutput.java
@@ -0,0 +1,67 @@
+package coneforest.psi;
+
+public class PsiOutput extends PsiObject
+{
+	public PsiOutput()
+	{
+	}
+
+	public PsiOutput(java.io.OutputStream output)
+	{
+		setOutput(output);
+	}
+
+	public String getTypeName()
+	{
+		return "output";
+	}
+
+	public String toString()
+	{
+		return "-output-";
+	}
+
+	public void setOutput(java.io.OutputStream output)
+	{
+		this.output=output;
+	}
+
+	public java.io.OutputStream getOutput()
+	{
+		return output;
+	}
+
+	public void write(int b)
+		throws PsiException
+	{
+		try
+		{
+			output.write(b);
+		}
+		catch(java.io.IOException e)
+		{
+			throw new PsiException("ioerror");
+		}
+	}
+
+	public void write(PsiInteger oByte)
+		throws PsiException
+	{
+		write(oByte.getValue().intValue());
+	}
+
+	public void flush()
+		throws PsiException
+	{
+		try
+		{
+			output.flush();
+		}
+		catch(java.io.IOException e)
+		{
+			throw new PsiException("ioerror");
+		}
+	}
+
+	private java.io.OutputStream output;
+}
