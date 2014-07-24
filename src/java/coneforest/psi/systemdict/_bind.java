e61a343a4be7d0f7e1d4582e746247ee5ca4e3a4
Anton Shvetz <tz@sectorb.msk.ru>
2014-07-24 04:44:27 +0400

    "bind" operator implemented

---
+++ src/java/coneforest/psi/systemdict/_bind.java
@@ -0,0 +1,50 @@
+package coneforest.psi.systemdict;
+import coneforest.psi.*;
+
+public class _bind extends PsiOperator
+{
+	public void execute(Interpreter interpreter)
+	{
+		OperandStack opstack=interpreter.getOperandStack();
+		if(opstack.size()<1)
+		{
+			interpreter.error("stackunderflow");
+			return;
+		}
+
+		PsiObject array=opstack.peek();
+
+		if(array instanceof PsiArray)
+			bind((PsiArray)array, interpreter.getDictionaryStack());
+		else
+			interpreter.error("typecheck");
+	}
+	
+	private static void bind(PsiArray array, DictionaryStack dictstack)
+	{
+		java.util.HashSet<PsiArray> bound=new java.util.HashSet<PsiArray>();
+		bindHelper(array, bound, dictstack);
+	}
+
+	private static void bindHelper(PsiArray array, java.util.HashSet<PsiArray> bound, DictionaryStack dictstack)
+	{
+		try
+		{
+			for(int i=0; i<((PsiArray)array).length(); i++)
+			{
+				PsiObject obj=((PsiArray)array).get(i);
+				if(obj instanceof PsiArray && bound.add((PsiArray)obj))
+					bindHelper((PsiArray)obj, bound, dictstack);
+				else if(obj instanceof PsiName && ((PsiName)obj).isExecutable())
+				{
+						PsiObject value=dictstack.load((PsiName)obj);
+						if(value instanceof PsiOperator)
+							((PsiArray)array).set(i, value);
+				}
+			}
+		}
+		catch(PsiException e)
+		{
+		}
+	}
+}
