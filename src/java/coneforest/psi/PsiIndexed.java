dd5b216209d832b6c5b36fd6d65a937e18bbb4d4
Anton Shvetz <tz@coneforest.msk.ru>
2014-12-06 20:56:26 +0300

    Documentation added

--- src/java/coneforest/psi/PsiIndexed.java
+++ src/java/coneforest/psi/PsiIndexed.java
@@ -1,10 +1,30 @@
 package coneforest.psi;
 
+/**
+*	A type of container whose elements are indexed.
+*	@param K A type of indices.
+*	@param V A type of elements.
+*/
 public interface PsiIndexed<K, V extends PsiObject>
 {
+	/**
+	*	Returns the element with given key or index.
+	*	@param key A key or an index.
+	*	@return An element.
+	*	@throws PsiException when index is out of range.
+	*/
 	public V psiGet(K key)
 		throws PsiException;
 
+	/**
+	*	Store an element with given key or index. In {@link PsiArraylike}
+	*	containers replaces existing element. In {@link PsiDictionarylike}
+	*	containers replaces an old or creates a new element associated with
+	*	specified key.
+	*	@param key A key or an index.
+	*	@param value An element to be stored with
+	*	@throws PsiException when index is out of range.
+	*/
 	public void psiPut(K key, V value)
 		throws PsiException;
 
