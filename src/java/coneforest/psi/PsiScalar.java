f0d0585eb72867199ec320977471bb6c0272852f
Anton Shvetz <tz@sectorb.msk.ru>
2014-12-06 20:56:26 +0300

    Documentation added

--- src/java/coneforest/psi/PsiScalar.java
+++ src/java/coneforest/psi/PsiScalar.java
@@ -1,14 +1,56 @@
 package coneforest.psi;
 
+/**
+ *	An interface bringing total ordering to implementing Ψ type.
+ *
+ *	@param T A type of object compared against this object.
+ */
 public interface PsiScalar<T extends PsiScalar>
 {
+	/**
+	 *	“Less” comparison.
+	 *
+	 *	@param scalar An object with which this object is compared.
+	 *	@return A Ψ boolean value indicating if this object is less than given
+	 *	object.
+	 */
 	public PsiBoolean psiLt(T scalar);
 
+	/**
+	 *	“Less or equal” comparison.
+	 *
+	 *	@param scalar An object with which this object is compared.
+	 *	@return A Ψ boolean value indicating if this object is less than or
+	 *	equal to given object.
+	 */
 	public PsiBoolean psiLe(T scalar);
 
+	/**
+	 *	“Greater” comparison.
+	 *
+	 *	@param scalar An object with which this object is compared.
+	 *	@return A Ψ boolean value indicating if this object is greater than
+	 *	given object.
+	 */
 	public PsiBoolean psiGt(T scalar);
 
+	/**
+	 *	“Greater or equal” comparison.
+	 *
+	 *	@param scalar An object with which this object is compared.
+	 *	@return A Ψ boolean value indicating if this object is greater than or
+	 *	equal to given object.
+	 */
 	public PsiBoolean psiGe(T scalar);
-	
+
+	/**
+	 *	Compares this object against given object and returns a Ψ integer
+	 *	indicating the result of the comparison. Returns negative value if this
+	 *	object is less than given one, zero if this object is equal to given
+	 *	one, and positive value if this object is greater than given one.
+	 *
+	 *	@param scalar An object with which this object is compared.
+	 *	@return A result of the comparison.
+	 */
 	public PsiInteger psiCmp(T scalar);
 }
