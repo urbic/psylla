790d6daa8f2deb5c6389dc53b42d45867733ce7f
Anton Shvetz <tz@sectorb.msk.ru>
2014-12-26 23:39:08 +0300

    "sort" operator now accepts PsiIterable&PsiLengthy

--- src/java/coneforest/psi/core/_sort.java
+++ src/java/coneforest/psi/core/_sort.java
@@ -14,29 +14,29 @@ public class _sort extends PsiOperator
 		}
 
 		final PsiObject proc=opstack.pop();
-		final PsiObject arraylike=opstack.pop();
+		final PsiObject iterable=opstack.pop();
 		try
 		{
-			final PsiObject[] a=new PsiObject[((PsiArraylike)arraylike).length()];
+			final PsiObject[] a=new PsiObject[((PsiLengthy)iterable).length()];
 			int i=0;
-			for(PsiObject obj: ((PsiArraylike<PsiObject>)arraylike))
+			for(PsiObject obj: ((PsiIterable<PsiObject>)iterable))
 				a[i++]=obj;
 
 			java.util.Arrays.sort(a, new java.util.Comparator<PsiObject>()
-				{
-					@Override
-					public int compare(PsiObject obj1, PsiObject obj2)
 					{
-						opstack.push(obj1);
-						opstack.push(obj2);
+						@Override
+						public int compare(PsiObject obj1, PsiObject obj2)
+						{
+							opstack.push(obj1);
+							opstack.push(obj2);
 
-						final int execLevel=interpreter.getExecLevel();
-						proc.invoke(interpreter);
-						interpreter.handleExecutionStack(execLevel);
+							final int execLevel=interpreter.getExecLevel();
+							proc.invoke(interpreter);
+							interpreter.handleExecutionStack(execLevel);
 
-						return ((PsiInteger)opstack.pop()).intValue();
-					}
-				});
+							return ((PsiInteger)opstack.pop()).intValue();
+						}
+					});
 
 			final PsiArray result=new PsiArray();
 			for(PsiObject obj: a)
@@ -47,7 +47,7 @@ public class _sort extends PsiOperator
 		}
 		catch(ClassCastException e)
 		{
-			opstack.push(arraylike);
+			opstack.push(iterable);
 			opstack.push(proc);
 			interpreter.error(e, this);
 		}
