909ecbfcda3670f6ac6d9b78aa75fe06bf7c93fa
Anton Shvetz <tz@coneforest.msk.ru>
2015-03-13 01:50:47 +0300

    "lsdir" operator renamed to "listdirectory". "listdirectory" operator reimplemented

--- src/java/coneforest/psi/core/_listdirectory.java
+++ src/java/coneforest/psi/core/_listdirectory.java
@@ -1,14 +1,15 @@
 package coneforest.psi.core;
 import coneforest.psi.*;
 
-public class _lsdir extends PsiOperator
+public class _listdirectory extends PsiOperator
 {
 	@Override
 	public void action(final Interpreter interpreter)
 		throws ClassCastException, PsiException
 	{
 		final OperandStack opstack=interpreter.getOperandStack();
-		String dirName=Utility.fileNameToNative(((PsiStringlike)opstack.popOperands(1)[0]).getString());
+		opstack.push(Utility.psiListDirectory((PsiStringlike)opstack.popOperands(1)[0]));
+		/*String dirName=Utility.fileNameToNative(((PsiStringlike)opstack.popOperands(1)[0]).getString());
 		try
 		{
 			String[] list=new java.io.File(dirName).list();
@@ -21,7 +22,8 @@ public class _lsdir extends PsiOperator
 		}
 		catch(SecurityException e)
 		{
-			throw new PsiException("security");
+			throw new PsiException("securityerror");
 		}
+		*/
 	}
 }
