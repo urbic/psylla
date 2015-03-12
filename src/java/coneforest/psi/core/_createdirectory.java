d6dc8d7f7ef11ffe631eac9fb786048ee5778b2f
Anton Shvetz <tz@coneforest.msk.ru>
2015-03-12 23:03:50 +0300

    "symlink" operator implemented. "mkdir" operator renamed to "createdirectory". "filesize", "issymlink", "createdirectory" operators reimplemented

--- src/java/coneforest/psi/core/_createdirectory.java
+++ src/java/coneforest/psi/core/_createdirectory.java
@@ -1,13 +1,13 @@
 package coneforest.psi.core;
 import coneforest.psi.*;
 
-public class _filesize extends PsiOperator
+public class _createdirectory extends PsiOperator
 {
 	@Override
 	public void action(final Interpreter interpreter)
 		throws ClassCastException, PsiException
 	{
 		final OperandStack opstack=interpreter.getOperandStack();
-		opstack.push(Utility.psiFileSize((PsiStringlike)opstack.popOperands(1)[0]));
+		Utility.psiCreateDirectory((PsiStringlike)opstack.popOperands(1)[0]);
 	}
 }
--- src/java/coneforest/psi/core/_issymlink.java
+++ src/java/coneforest/psi/core/_issymlink.java
@@ -8,6 +8,6 @@ public class _issymlink extends PsiOperator
 		throws ClassCastException, PsiException
 	{
 		final OperandStack opstack=interpreter.getOperandStack();
-		opstack.push(Utility.psiIsSymlink(((PsiStringlike)opstack.popOperands(1)[0])));
+		opstack.push(Utility.psiIsSymLink(((PsiStringlike)opstack.popOperands(1)[0])));
 	}
 }
