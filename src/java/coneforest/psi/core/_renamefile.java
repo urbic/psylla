4baa6b41e69be9f096659e812f5733c70ac99790
Anton Shvetz <tz@sectorb.msk.ru>
2015-03-12 22:14:05 +0300

    "isdir" operator renamed to "isdirectory". "isfile", "isdirectory", "fileexists" operators reimplemented. "issymlink", "renamefile" operators implemented

--- src/java/coneforest/psi/core/_renamefile.java
+++ src/java/coneforest/psi/core/_renamefile.java
@@ -1,13 +1,16 @@
 package coneforest.psi.core;
 import coneforest.psi.*;
 
-public class _fileexists extends PsiOperator
+public class _renamefile extends PsiOperator
 {
 	@Override
 	public void action(final Interpreter interpreter)
 		throws ClassCastException, PsiException
 	{
 		final OperandStack opstack=interpreter.getOperandStack();
-		opstack.push(Utility.psiFileExists(((PsiStringlike)opstack.popOperands(1)[0])));
+		PsiObject[] ops=opstack.popOperands(2);
+		PsiStringlike oldName=(PsiStringlike)ops[0];
+		PsiStringlike newName=(PsiStringlike)ops[1];
+		Utility.psiRenameFile(oldName, newName);
 	}
 }
