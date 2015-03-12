8755c8b42e04d67a75e376d986ae3cf06d4d2a91
Anton Shvetz <tz@sectorb.msk.ru>
2015-03-12 21:17:19 +0300

    "deletefile", "readlink" operators implemented

--- src/java/coneforest/psi/Utility.java
+++ src/java/coneforest/psi/Utility.java
@@ -24,4 +24,53 @@ public class Utility
 			throw new PsiException("interrupt");
 		}
 	}
+
+	public static void psiDeleteFile(final PsiStringlike stringlike)
+		throws PsiException
+	{
+		String fileName=fileNameToNative(stringlike.getString());
+		try
+		{
+			java.nio.file.Files.delete(new java.io.File(fileName).toPath());
+		}
+		catch(java.nio.file.NoSuchFileException e)
+		{
+			throw new PsiException("undefinedfilename");
+		}
+		catch(java.nio.file.DirectoryNotEmptyException e)
+		{
+			throw new PsiException("directorynotempty");
+		}
+		catch(java.lang.SecurityException e)
+		{
+			throw new PsiException("securityerror");
+		}
+		catch(java.io.IOException e)
+		{
+			throw new PsiException("ioerror");
+		}
+	}
+
+	public static PsiString psiReadLink(final PsiStringlike stringlike)
+		throws PsiException
+	{
+		String linkName=fileNameToNative(stringlike.getString());
+		try
+		{
+			return new PsiString(java.nio.file.Files.readSymbolicLink(
+					new java.io.File(linkName).toPath()).toString());
+		}
+		catch(java.nio.file.NotLinkException e)
+		{
+			throw new PsiException("notlink");
+		}
+		catch(java.lang.SecurityException e)
+		{
+			throw new PsiException("securityerror");
+		}
+		catch(java.io.IOException e)
+		{
+			throw new PsiException("ioerror");
+		}
+	}
 }
---
+++ src/java/coneforest/psi/core/_readlink.java
@@ -0,0 +1,13 @@
+package coneforest.psi.core;
+import coneforest.psi.*;
+
+public class _readlink extends PsiOperator
+{
+	@Override
+	public void action(final Interpreter interpreter)
+		throws ClassCastException, PsiException
+	{
+		final OperandStack opstack=interpreter.getOperandStack();
+		opstack.push(Utility.psiReadLink((PsiStringlike)opstack.popOperands(1)[0]));
+	}
+}
