4120475968a128b4d4399763f76e5c95df210a8e
Anton Shvetz <tz@sectorb.msk.ru>
2015-03-16 18:56:29 +0300

    "signalerror" operator reimplemented and documented

--- src/java/coneforest/psi/core/_signalerror.java
+++ src/java/coneforest/psi/core/_signalerror.java
@@ -5,25 +5,10 @@ public class _signalerror extends PsiOperator
 {
 	@Override
 	public void action(final Interpreter interpreter)
-	{
-	}
-
-	@Override
-	public void invoke(final Interpreter interpreter)
+		throws ClassCastException, PsiException
 	{
 		final OperandStack opstack=interpreter.getOperandStack();
-		try
-		{
-			final PsiObject[] ops=opstack.popOperands(2);
-			interpreter.handleError(((PsiStringlike)ops[1]).getString(), ops[0]);
-		}
-		catch(ClassCastException e)
-		{
-			interpreter.handleError("typecheck", this);
-		}
-		catch(PsiException e)
-		{
-			interpreter.handleError(e.kind(), this);
-		}
+		final PsiObject[] ops=opstack.popOperands(2);
+		interpreter.handleError(((PsiStringlike)ops[1]).getString(), ops[0]);
 	}
 }
