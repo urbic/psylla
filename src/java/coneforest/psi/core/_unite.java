f3dbc48a807cfe6c90b6be554eeac9f453cd956d
Anton Shvetz <tz@sectorb.msk.ru>
2015-03-22 00:08:22 +0300

    "unite" operator implemented and documented

---
+++ src/java/coneforest/psi/core/_unite.java
@@ -0,0 +1,14 @@
+package coneforest.psi.core;
+import coneforest.psi.*;
+
+public class _unite extends PsiOperator
+{
+	@Override
+	public void action(final Interpreter interpreter)
+		throws ClassCastException, PsiException
+	{
+		final OperandStack opstack=interpreter.getOperandStack();
+		final PsiObject[] ops=opstack.popOperands(2);
+		opstack.push(((PsiArray)ops[0]).psiUnite((PsiStringlike)ops[1]));
+	}
+}
