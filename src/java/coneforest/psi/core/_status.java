d30200232e86a97478dd5ce966f135196d19d201
Anton Shvetz <tz@sectorb.msk.ru>
2015-06-28 00:01:35 +0300

    Implement "status" operator

---
+++ src/java/coneforest/psi/core/_status.java
@@ -0,0 +1,13 @@
+package coneforest.psi.core;
+import coneforest.psi.*;
+
+public class _status extends PsiOperator
+{
+	@Override
+	public void action(final Interpreter interpreter)
+		throws ClassCastException, PsiException
+	{
+		final OperandStack opstack=interpreter.getOperandStack();
+		opstack.push(((PsiProcess)opstack.popOperands(1)[0]).psiStatus());
+	}
+}
