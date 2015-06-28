0e413bbdca2ceb0de42e62c76d9b3947f89e660f
Anton Shvetz <tz@coneforest.msk.ru>
2015-06-28 23:14:58 +0300

    PsiException now implements PsiObject

--- src/java/coneforest/psi/PsiException.java
+++ src/java/coneforest/psi/PsiException.java
@@ -2,6 +2,7 @@ package coneforest.psi;
 
 public class PsiException
 	extends Exception
+	implements PsiObject
 {
 	public PsiException(final String kind)
 	{
@@ -11,6 +12,60 @@ public class PsiException
 	public String kind()
 	{
 		return kind;
+	}
+
+	@Override
+	public PsiBoolean psiIsA(PsiStringlike stringlike)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public PsiString psiToString()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public PsiException psiClone()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public PsiBoolean psiNe(PsiObject obj)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public PsiBoolean psiEq(PsiObject obj)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void invoke(Interpreter interpreter)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void execute(Interpreter interpreter)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public PsiCommand psiType()
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String getTypeName()
+	{
+		throw new UnsupportedOperationException();
 	}
 
 	private String kind;
