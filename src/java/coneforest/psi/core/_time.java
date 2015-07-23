04177f418c784d5ff85e5f9bd720babb6b3ddf52
Anton Shvetz <tz@coneforest.msk.ru>
2015-07-23 15:03:36 +0300

    Reimplement "time" operator

--- src/java/coneforest/psi/core/_time.java
+++ src/java/coneforest/psi/core/_time.java
@@ -6,6 +6,7 @@ public class _time extends PsiOperator
 	@Override
 	public void action(final Interpreter interpreter)
 	{
-		interpreter.getOperandStack().push(new PsiInteger((new java.util.Date()).getTime()));
+		//interpreter.getOperandStack().push(new PsiInteger((new java.util.Date()).getTime()));
+		interpreter.getOperandStack().push(new PsiInteger(System.currentTimeMillis()));
 	}
 }
