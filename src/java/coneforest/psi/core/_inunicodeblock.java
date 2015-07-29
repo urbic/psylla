c6ff31982a32a30c5fcdf8863425319d4cbea55d
Anton Shvetz <tz@coneforest.msk.ru>
2015-07-30 02:34:43 +0300

    Implement "inunicodeblock" operator

---
+++ src/java/coneforest/psi/core/_inunicodeblock.java
@@ -0,0 +1,14 @@
+package coneforest.psi.core;
+import coneforest.psi.*;
+
+public class _inunicodeblock extends PsiOperator
+{
+	@Override
+	public void action(final Interpreter interpreter)
+		throws PsiException
+	{
+		final OperandStack opstack=interpreter.getOperandStack();
+		final PsiObject[] ops=opstack.popOperands(2);
+		opstack.push(((PsiInteger)ops[0]).psiInUnicodeBlock((PsiStringy)ops[1]));
+	}
+}
