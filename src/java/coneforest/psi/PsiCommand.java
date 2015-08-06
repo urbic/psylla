a5d88d37f370d625aa954206afcbc5ce08416c3d
Anton Shvetz <tz@sectorb.msk.ru>
2015-08-06 03:20:19 +0300

    Unsuccessfull attempt to fix the issue

--- src/java/coneforest/psi/PsiCommand.java
+++ src/java/coneforest/psi/PsiCommand.java
@@ -13,7 +13,11 @@ public class PsiCommand
 	{
 		try
 		{
+			//final int execLevel=interpreter.getExecLevel();
 			interpreter.getDictStack().load(this).invoke(interpreter);
+			//interpreter.getExecutionStack().push(interpreter.getDictStack().load(this));
+			//System.out.println(getString()+ " invoked");
+			//interpreter.handleExecutionStack(execLevel);
 		}
 		catch(PsiException e)
 		{
