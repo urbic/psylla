3b8c698d9849e1ac1befb877ca53b8b006fbac50
Anton Shvetz <tz@sectorb.msk.ru>
2016-02-06 06:27:56 +0300

    Now PsiCondition does not implement PsiAtomic

--- src/java/coneforest/psi/PsiCondition.java
+++ src/java/coneforest/psi/PsiCondition.java
@@ -4,7 +4,7 @@ package coneforest.psi;
  *	A representation of Ψ-{@code condition} object.
  */
 public class PsiCondition
-	implements PsiAtomic
+	implements PsiObject
 {
 	/**
 	 *	@return a string {@code "condition"}.
