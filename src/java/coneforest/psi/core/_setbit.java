b67d258019f55c88792a8ea50f80f3ef33415c7e
Anton Shvetz <tz@sectorb.msk.ru>
2016-02-11 02:47:20 +0300

    Implement "clearbit", "flipbit", "setbit", "clearbit" opeartors. Introduce PsiBitwise

--- src/java/coneforest/psi/core/_setbit.java
+++ src/java/coneforest/psi/core/_setbit.java
@@ -1,7 +1,7 @@
 package coneforest.psi.core;
 import coneforest.psi.*;
 
-public final class _bitshift extends PsiOperator
+public final class _setbit extends PsiOperator
 {
 	@Override
 	public void action(final Interpreter interpreter)
@@ -9,6 +9,6 @@ public final class _bitshift extends PsiOperator
 	{
 		final OperandStack ostack=interpreter.operandStack();
 		final PsiObject[] ops=ostack.popOperands(2);
-		ostack.push(((PsiBitwise)ops[0]).psiBitShift((PsiInteger)ops[1]));
+		ostack.push(((PsiBitwise)ops[0]).psiSetBit((PsiInteger)ops[1]));
 	}
 }
