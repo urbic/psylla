807a612fc649c5431f1e14d65f0e0b6da7592308
Anton Shvetz <shvetz.anton@gmail.com>
2021-09-25 11:51:09 +0300

    Add module-info.java

---
+++ src/main/java/coneforest/psylla/module-info.java
@@ -0,0 +1,10 @@
+module coneforest.psylla
+{
+	exports coneforest.psylla;
+	exports coneforest.psylla.tools;
+	requires java.scripting;
+	requires java.compiler;
+	requires ant.junit;
+	requires junit4;
+	requires jline;
+}
