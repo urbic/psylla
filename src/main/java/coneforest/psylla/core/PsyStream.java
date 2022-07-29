1f5bc82a34ff2395fd13ed607a2ebbfd91b701bc
Anton Shvetz <shvetz.anton@gmail.com>
2022-07-30 02:59:09 +0300

    [impl,doc]
    - Implement --streamtomark--
    - Update documentation

--- src/main/java/coneforest/psylla/core/PsyStream.java
+++ src/main/java/coneforest/psylla/core/PsyStream.java
@@ -16,11 +16,6 @@ public class PsyStream
 		return new PsyStream(Stream.<PsyObject>iterate(o, oProc.asUnaryOperator(oContext)));
 	}
 
-	/*public PsyStream psyDistinct()
-	{
-		return new PsyStream(Stream.<PsyObject>distict());
-	}*/
-
 	@Override
 	public Stream<? extends PsyObject> stream()
 	{
@@ -38,7 +33,18 @@ public class PsyStream
 						final var ostack=oContext.operandStackBacked(2);
 						ostack.push(PsyStream.psyIterate(ostack.getBacked(0), ostack.getBacked(1), oContext));
 					}),
-			//new PsyOperator.Arity11<PsyStream>
-			//	("distinct", PsyStream::psyDistinct),
+			new PsyOperator.Action
+				("streamtomark",
+					(oContext)->
+					{
+						final var ostack=oContext.operandStack();
+						final var i=ostack.findMarkPosition()+1;
+						final var s=ostack.size();
+						final var objects=new PsyObject[s-i];
+						for(int j=i; j<s; j++)
+							objects[j-i]=ostack.get(j);
+						ostack.setSize(i-1);
+						ostack.push(new PsyStream(Stream.of(objects)));
+					}),
 		};
 }
