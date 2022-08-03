132bfcdaaf44f1194886feb3f1106cbb3c573010
Anton Shvetz <shvetz.anton@gmail.com>
2022-08-03 09:01:37 +0300

    [impl] Breaking:
    - Remove --capturegroup{start,end}--
    - Change signature of --capturegroup--

--- src/main/java/coneforest/psylla/core/PsyMatcher.java
+++ src/main/java/coneforest/psylla/core/PsyMatcher.java
@@ -35,34 +35,6 @@ public class PsyMatcher
 		return PsyInteger.valueOf(matcher.groupCount());
 	}
 
-	public PsyString psyCaptureGroup(final PsyObject oKey)
-		throws PsyException
-	{
-		try
-		{
-			final String group;
-			if(oKey instanceof PsyInteger)
-				group=matcher.group(((PsyInteger)oKey).intValue());
-			else if(oKey instanceof PsyTextual)
-				group=matcher.group(((PsyTextual)oKey).stringValue());
-			else
-				throw new PsyTypeCheckException();
-			return group!=null? new PsyString(group): null;
-		}
-		catch(final IndexOutOfBoundsException e)
-		{
-			throw new PsyRangeCheckException();
-		}
-		catch(final IllegalArgumentException e)
-		{
-			throw new PsyUndefinedException();
-		}
-		catch(final IllegalStateException e)
-		{
-			throw new PsyInvalidStateException();
-		}
-	}
-
 	public PsyInteger psyCaptureGroupStart(final PsyObject oKey)
 		throws PsyException
 	{
@@ -121,4 +93,59 @@ public class PsyMatcher
 
 	private final java.util.regex.Matcher matcher;
 
+	public static final PsyOperator[] OPERATORS=
+		{
+			new PsyOperator.Action
+				("capturegroup", (oContext)->
+					{
+						final var ostack=oContext.operandStackBacked(2);
+						try
+						{
+							final var matcher=((PsyMatcher)ostack.getBacked(0)).matcher;
+							final var oKey=ostack.getBacked(1);
+							if(oKey instanceof PsyInteger)
+							{
+								final var index=((PsyInteger)oKey).intValue();
+								final var group=matcher.group(index);
+								if(group!=null)
+								{
+									ostack.push(new PsyName(group));
+									ostack.push(PsyInteger.valueOf(matcher.start(index)));
+									ostack.push(PsyInteger.valueOf(matcher.end(index)));
+								}
+								ostack.push(PsyBoolean.valueOf(group!=null));
+							}
+							else if(oKey instanceof PsyTextual)
+							{
+								final var key=((PsyTextual)oKey).stringValue();
+								final var group=matcher.group(key);
+								if(group!=null)
+								{
+									ostack.push(new PsyName(group));
+									ostack.push(PsyInteger.valueOf(matcher.start(key)));
+									ostack.push(PsyInteger.valueOf(matcher.end(key)));
+								}
+								ostack.push(PsyBoolean.valueOf(group!=null));
+							}
+							else
+								throw new PsyTypeCheckException();
+						}
+						catch(final IndexOutOfBoundsException e)
+						{
+							throw new PsyRangeCheckException();
+						}
+						catch(final IllegalArgumentException e)
+						{
+							throw new PsyUndefinedException();
+						}
+						catch(final IllegalStateException e)
+						{
+							throw new PsyInvalidStateException();
+						}
+					}),
+			new PsyOperator.Arity11<PsyMatcher>("capturegroupcount", PsyMatcher::psyCaptureGroupCount),
+			new PsyOperator.Arity21<PsyTextual, PsyRegExp>("matcher", PsyMatcher::new),
+			new PsyOperator.Arity11<PsyMatcher>("matches", PsyMatcher::psyMatches),
+			new PsyOperator.Arity21<PsyMatcher, PsyTextual>("replaceall", PsyMatcher::psyReplaceAll),
+		};
 }
