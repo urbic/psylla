d7dbde081d6105324aab332366a35499dcf4f886
urbic <shvetz.anton@gmail.com>
2023-01-22 17:16:23 +0000

    Deploying to gh-pages from  @ cae343cbcd8259280d08eb75ad9095600c74eaee 🚀

---
+++ examples/java/coneforest/psylla/PsyllaToolProvider.java
@@ -0,0 +1,14 @@
+package coneforest.psylla;
+
+import java.io.PrintWriter;
+import java.util.ServiceLoader;
+import java.util.spi.ToolProvider;
+
+public class PsyllaToolProvider
+{
+	public static void main(final String[] args)
+	{
+		var tool=ToolProvider.findFirst("psylla").get();
+		tool.run((PrintWriter)null, (PrintWriter)null, "-e", "1 1 10 { ? } for");
+	}
+}
